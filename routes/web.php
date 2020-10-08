a2ad4e10a38003e26af92ba270822af3edf5efb0
pacha2880 <salguero_ro@hotmail.com>
2020-10-08 11:47:40 -0400

    rutas con nombre

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,16 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "<a href='". route('contactos') ."'>Contacto 1</a><br>";
+    echo "<a href='". route('contactos') ."'>Contacto 2</a><br>";
+    echo "<a href='". route('contactos') ."'>Contacto 3</a><br>";
+    echo "<a href='". route('contactos') ."'>Contacto 4</a><br>";
+});
+
+ROute::get('contactame', function(){
+    return "seccion de contactos";
+})->name('contactos');
+
+Route::get('caca/{nombre?}', function($nombre = "invitado") {
+    return "hola " . $nombre . " este mundo es una caca";
 });
