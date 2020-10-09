bc4b7a524059fb89d00b4844e97208cd66dbd7bf
pacha2880 <salguero_ro@hotmail.com>
2020-10-08 22:06:13 -0400

    PortfolioController

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-$portfolio = [
-    ['title' => 'Proyecto #1'],
-    ['title' => 'Proyecto #2'],
-    ['title' => 'Proyecto #3'],
-    ['title' => 'Proyecto #4']
-];
+
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::view('/portfolio', 'portfolio', compact('portfolio'))->name('potfolio');
-Route::view('/contact', 'contact')->name('contact');
+Route::get('/portfolio', 'PortfolioController@index')->name('potfolio');
+yRoute::view('/contact', 'contact')->name('contact');
+
+// Route::resource('projectos', 'PortfolioController');e('contact');
