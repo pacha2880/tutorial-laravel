632fa56961b993d0fe1693da9ecbfaa26f76f4b4
pacha2880 <salguero_ro@hotmail.com>
2020-10-09 12:21:13 -0400

    validar formularios

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,12 +4,24 @@
 
 @section('content')
     <h1>Contact</h1>
+    @if ($errors->any())
+        {{ $errors->first('name') }}
+        <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+        </ul>
+    @endif
     <form method="POST" action="{{ route('contact') }}">
         @csrf
-        <input name="name" placeholder="Nombre..." value=""><br>
-        <input name="email" placeholder="Email..." value="caco@cagones.com"><br>
-        <input name="subject" placeholder="Asunto..." value="asunto de caca"><br>
-        <textarea name="content" placeholder="Mensaje...">mensaje de caca</textarea><br>
+        <input name="name" placeholder="Nombre..." value="{{ old('name') }}"><br>
+        {!! $errors->first('name', '<small>:message</small><br>') !!}
+        <input type="text" name="email" placeholder="Email..." value="{{ old('email') }}"><br>
+        {!! $errors->first('email', '<small>:message</small><br>') !!}
+        <input name="subject" placeholder="Asunto..." value="{{ old('subject') }}"><br>
+        {!! $errors->first('subject', '<small>:message</small><br>') !!}
+        <textarea name="content" placeholder="Mensaje...">{{ old('content') }}</textarea><br>
+        {!! $errors->first('content', '<small>:message</small><br>') !!}
         <button>Enviar</button>
     </form>
 @endsection
