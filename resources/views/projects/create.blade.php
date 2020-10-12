8854bd73dc96bc5d7ae0b9cee18e1a8a44986ba8
pacha2880 <salguero_ro@hotmail.com>
2020-10-11 23:09:19 -0400

    reutilizar formulario

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -5,32 +5,12 @@
 @section('content')
     <h1>Crear proyecto</h1>
 
-    @if($errors->any())
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
+    @include('partials.validation-errors')
 
     <form method="POST" action="{{ route('projects.store') }}">
-        @csrf
-        <label>
-            Titulo del proyecto <br>
-            <input type="text" name="title" value="{{ old('title') }}">
-        </label>
-        <br>
-        <label>
-            URL del proyecto <br>
-            <input type="text" name="url" value="{{ old('url') }}">
-        </label>
-        <br>
-        <label>
-            Descripcion del proyecto <br>
-            <textarea name="description">{{ old('description') }}</textarea>
-        </label>
-        <br>
-        <button>Guardar</button>
+        
+        @include('projects._form', ['btnText' => 'Guardar'])
+
     </form>
 
 @endsection
