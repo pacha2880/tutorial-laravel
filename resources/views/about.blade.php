a3286920e6381be8d53715c048f7b60f1897b425
pacha2880 <salguero_ro@hotmail.com>
2020-10-29 23:41:44 -0400

    fin parte principiante

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,5 +3,23 @@
 @section('title', 'About')
 
 @section('content')
-    <h1>About</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-lg-6">
+            <img class="img-fluid mb-4" src="/img/about.svg" alt="Desarrollo Web">
+        </div>
+        <div class="col-12 col-lg-6">
+            <h1 class="display-4 text-primary">Quien Soy</h1>
+            <p class="lead-text-secondary">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic quo quas doloremque. Libero voluptates eveniet tenetur amet incidunt nulla quia, magni adipisci! Fuga ratione sequi ea qui facilis a recusandae?
+            </p>
+            <a class="btn btn-lg btn-block btn-primary" 
+                href="{{ route('contact') }}"
+            >Contactame</a>
+            <a class="btn btn-lg btn-block btn-outline-primary" 
+                href="{{ route('projects.index') }}"
+            >Portafolio</a>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-12">
+        <div class="col-12 col-lg-6">
             <h1 class="display-4 text-primary">Desarrollo Web</h1>
             <p class="lead-text-secondary">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic quo quas doloremque. Libero voluptates eveniet tenetur amet incidunt nulla quia, magni adipisci! Fuga ratione sequi ea qui facilis a recusandae?
@@ -16,6 +16,9 @@
             <a class="btn btn-lg btn-block btn-outline-primary" 
                 href="{{ route('projects.index') }}"
             >Portafolio</a>
+        </div>
+        <div class="col-12 col-lg-6">
+            <img class="img-fluid mb-4" src="/img/home.svg" alt="Desarrollo Web">
         </div>
     </div>
 </div>
